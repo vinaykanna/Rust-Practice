456f75b05f7090d5594c0f7a694fb6033d3f4e88
vinaykanna <vinaykanna367@gmail.com>
2024-02-13 02:03:57 +0530

    add basic projects for practice

--- guessing_game/target/debug/deps/fundamentals-572f9b49555e3d2d.d
+++ guessing_game/target/debug/deps/fundamentals-572f9b49555e3d2d.d
@@ -1,5 +1,5 @@
-/Users/vinay.kumar/Desktop/Learnings/Rust/guessing_game/target/debug/deps/fundamentals-572f9b49555e3d2d.rmeta: src/main.rs
+/Users/vinay.kumar/Desktop/Learnings/Rust-Practice/guessing_game/target/debug/deps/fundamentals-572f9b49555e3d2d.rmeta: src/main.rs
 
-/Users/vinay.kumar/Desktop/Learnings/Rust/guessing_game/target/debug/deps/fundamentals-572f9b49555e3d2d.d: src/main.rs
+/Users/vinay.kumar/Desktop/Learnings/Rust-Practice/guessing_game/target/debug/deps/fundamentals-572f9b49555e3d2d.d: src/main.rs
 
 src/main.rs:
--- guessing_game/target/debug/deps/fundamentals-bb3b7da713834ac2.d
+++ guessing_game/target/debug/deps/fundamentals-bb3b7da713834ac2.d
@@ -1,5 +1,5 @@
-/Users/vinay.kumar/Desktop/Learnings/Rust/guessing_game/target/debug/deps/fundamentals-bb3b7da713834ac2.rmeta: src/main.rs
+/Users/vinay.kumar/Desktop/Learnings/Rust-Practice/guessing_game/target/debug/deps/fundamentals-bb3b7da713834ac2.rmeta: src/main.rs
 
-/Users/vinay.kumar/Desktop/Learnings/Rust/guessing_game/target/debug/deps/fundamentals-bb3b7da713834ac2.d: src/main.rs
+/Users/vinay.kumar/Desktop/Learnings/Rust-Practice/guessing_game/target/debug/deps/fundamentals-bb3b7da713834ac2.d: src/main.rs
 
 src/main.rs:
